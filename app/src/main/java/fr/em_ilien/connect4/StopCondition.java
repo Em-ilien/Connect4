456f88cae88c4e3859edcb9804e5920fb8c3505a
Emilien Cosson <emilien@em-ilien.fr>
2024-05-19 15:12:04 +0200

    refactor StopCondition class

--- app/src/main/java/fr/em_ilien/connect4/StopCondition.java
+++ app/src/main/java/fr/em_ilien/connect4/StopCondition.java
@@ -14,30 +14,42 @@ public class StopCondition {
 	}
 
 	public Color getWinner() {
+		final Color color = checkIfWinnerOnHorinzontalLines();
+		if (color != null)
+			return color;
+		return null;
+	}
+
+	private Color checkIfWinnerOnHorinzontalLines() {
 		for (int i = 0; i < Grid.COLUMNS_NUMBER; i++) {
 			if (this.grid.getColumn(MIDDLE_COLUMN).getTokenAtRow(i) == null)
 				continue;
 			final Color color = this.grid.getColumn(MIDDLE_COLUMN).getTokenAtRow(i).getColor();
+
 			final List<Integer> coloredCell = new ArrayList<>();
 			coloredCell.add(MIDDLE_COLUMN);
-			int j = MIDDLE_COLUMN;
-			do {
-				j--;
-				if (this.grid.getColumn(j).getTokenAtRow(i) == null || this.grid.getColumn(j).getTokenAtRow(i).getColor() != color)
-					break;
-				coloredCell.add(j);
-			} while (Math.abs(MIDDLE_COLUMN - j) < REQUIRED_ALIGNED_TOKENS_TO_WIN - 1);
-			j = MIDDLE_COLUMN;
-			do {
-				j++;
-				if (this.grid.getColumn(j).getTokenAtRow(i) == null || this.grid.getColumn(j).getTokenAtRow(i).getColor() != color)
-					break;
-				coloredCell.add(j);
-			} while (Math.abs(MIDDLE_COLUMN - j) < REQUIRED_ALIGNED_TOKENS_TO_WIN - 1);
+			coloredCell.addAll(fetchTokenWithSameColorInRow(i, color, true));
+			coloredCell.addAll(fetchTokenWithSameColorInRow(i, color, false));
 
 			if (coloredCell.size() >= REQUIRED_ALIGNED_TOKENS_TO_WIN)
 				return color;
 		}
 		return null;
+	}
+
+	private List<Integer> fetchTokenWithSameColorInRow(int row, final Color color, boolean checkLeftColumns) {
+		final List<Integer> coloredCell = new ArrayList<>();
+		int column = MIDDLE_COLUMN;
+		do {
+			if (checkLeftColumns)
+				column--;
+			else
+				column++;
+			final Token token = this.grid.getColumn(column).getTokenAtRow(row);
+			if (token == null || token.getColor() != color)
+				break;
+			coloredCell.add(column);
+		} while (Math.abs(MIDDLE_COLUMN - column) < REQUIRED_ALIGNED_TOKENS_TO_WIN - 1);
+		return coloredCell;
 	}
 }
